bcabd7d788f88e30e9f97c700297760f77854330
Crowdbotics <engineering@crowdbotics.com>
2023-08-24 17:26:44 +0000

    Initial commit.

---
+++ backend/rob0824_dev_111211/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for rob0824_dev_111211 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rob0824_dev_111211.settings')
+
+application = get_wsgi_application()
